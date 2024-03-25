e55c06b5fed4080274923aca10adc70c7efffe83
Joy-Renee <152525597+Joy-Renee@users.noreply.github.com>
2024-03-25 11:15:15 +0300

    solution2.js

--- solution2.js
+++ solution2.js
@@ -5,40 +5,40 @@ function inputSpeed(speed){
     if(speed <= 70){
         return 'Ok'
     }
-    else if(speed === 75){
+    else if(speed >= 75 && speed < 80){
         return 'Point: 1 '
     }
-    else if(speed === 80){
+    else if(speed >= 80 && speed < 85){
         return 'Points: 2 '
     }
-    else if(speed === 85){
+    else if(speed >= 85 && speed < 90){
         return 'Points: 3 '
     }
-    else if(speed === 90){
+    else if(speed >= 90 && speed < 95){
         return 'Points: 4 '
     }
-    else if(speed === 95){
+    else if(speed >= 95 && speed < 100){
         return 'Points: 5 '
     }
-    else if(speed === 100){
+    else if(speed >= 100 && speed < 105){
         return 'Points: 6 '
     }
-    else if(speed === 105){
+    else if(speed >= 105 && speed < 110){
         return 'Points: 7 '
     }
-    else if(speed === 110){
+    else if(speed >= 110 && speed < 115){
         return 'Points: 8 '
     }
-    else if(speed === 115){
+    else if(speed >= 115 && speed < 120){
         return 'Points: 9 '
     }
-    else if(speed === 120){
+    else if(speed >= 120 && speed < 125){
         return 'Points: 10 '
     }
-    else if(speed === 125){
+    else if(speed >= 125 && speed < 130){
         return 'Points: 11 '
     }
-    else if(speed === 130){
+    else if(speed >= 130 && speed < 135){
         return 'Points: 12 '
     }
     else if(speed >= 135){
